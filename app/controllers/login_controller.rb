ec5b92d0638ed265cf66ccebdcc8841510657b8c
samaswin87 <aswin.r312@outlook.com>
2019-12-05 12:18:11 +0530

    added devise pages

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class LoginController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    root to: "users/sessions#new"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/home', to: "home#index"
 end
